252005982235a590afd6a4e0acda8d3058f990c4
Radamés Roriz <radamesroriz@gmail.com>
2012-07-03 11:32:48 -0300

    Versão 1
    Provavelmente existem alguns erros ainda, mas essa é apenas a primeira
    versão

--- pt-br.js
+++ pt-br.js
@@ -1,24 +1,265 @@
 ﻿tinyMCE.addI18n({
-	en:{
+	pt-br:{
 		common:{
-			"more_colors":"More Colors...",
-			"invalid_data":"Erro: Invalid values entered, these are marked in red.",
+			"more_colors":"Mais Cores...",
+			"invalid_data":"Erro: Entrada de dados inválida, os mesmos foram marcados em vermelho.",
 			"popup_blocked":"Desculpe, mas notamos que seu navegador bloqueou o pop-up. Para utilizar plenamente esta ferramente abilite o pop-up em seu navegador.",
 			"clipboard_no_support":"Atualmente seu navegador não suporta essa função, utilize atalhos de teclado. Currently not supported by your browser, use keyboard shortcuts instead.",
 			"clipboard_msg":"Copiar/Cortar/Colar não é funcional no Mozilla e Firefox.\nVocê gostaria mais informações sobre este problema?",
-			"not_set":"-- Not Set --",
+			"not_set":"-- Não Informado --",
 			"class_name":"Classe",
 			browse:"Procurar",
-			close:"Close",
-			cancel:"Cancel",
-			update:"Update",
-			insert:"Insert",
-			apply:"Apply",
-			"edit_confirm":"Do you want to use the WYSIWYG mode for this textarea?",
-			"invalid_data_number":"{#field} must be a number",
-			"invalid_data_min":"{#field} must be a number greater than {#min}",
-			"invalid_data_size":"{#field} must be a number or percentage",
-			value:"(value)"
+			close:"Fechar",
+			cancel:"Cancelado",
+			update:"Atualizar",
+			insert:"Inserir",
+			apply:"Aplicar",
+			"edit_confirm":"Gostaria de usar o editor de html para este textarea?",
+			"invalid_data_number":"{#field} deve ser um número",
+			"invalid_data_min":"{#field} deve ser um número maior do que {#min}",
+			"invalid_data_size":"{#field} deve ser um número ou a percentagem",
+			value:"(valor)"
 		},
 		contextmenu:{
-			full:"Full",right:"Right",center:"Center",left:"Left",align:"Alignment"},insertdatetime:{"day_short":"Sun,Mon,Tue,Wed,Thu,Fri,Sat,Sun","day_long":"Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday","months_short":"Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec","months_long":"January,February,March,April,May,June,July,August,September,October,November,December","inserttime_desc":"Insert Time","insertdate_desc":"Insert Date","time_fmt":"%H:%M:%S","date_fmt":"%Y-%m-%d"},print:{"print_desc":"Print"},preview:{"preview_desc":"Preview"},directionality:{"rtl_desc":"Direction Right to Left","ltr_desc":"Direction Left to Right"},layer:{content:"New layer...","absolute_desc":"Toggle Absolute Positioning","backward_desc":"Move Backward","forward_desc":"Move Forward","insertlayer_desc":"Insert New Layer"},save:{"save_desc":"Save","cancel_desc":"Cancel All Changes"},nonbreaking:{"nonbreaking_desc":"Insert Non-Breaking Space Character"},iespell:{download:"ieSpell not detected. Do you want to install it now?","iespell_desc":"Check Spelling"},advhr:{"delta_height":"","delta_width":"","advhr_desc":"Insert Horizontal Line"},emotions:{"delta_height":"","delta_width":"","emotions_desc":"Emotions"},searchreplace:{"replace_desc":"Find/Replace","delta_width":"","delta_height":"","search_desc":"Find"},advimage:{"delta_width":"","image_desc":"Insert/Edit Image","delta_height":""},advlink:{"delta_height":"","delta_width":"","link_desc":"Insert/Edit Link"},xhtmlxtras:{"attribs_delta_height":"","attribs_delta_width":"","ins_delta_height":"","ins_delta_width":"","del_delta_height":"","del_delta_width":"","acronym_delta_height":"","acronym_delta_width":"","abbr_delta_height":"","abbr_delta_width":"","cite_delta_height":"","cite_delta_width":"","attribs_desc":"Insert/Edit Attributes","ins_desc":"Insertion","del_desc":"Deletion","acronym_desc":"Acronym","abbr_desc":"Abbreviation","cite_desc":"Citation"},style:{"delta_height":"","delta_width":"",desc:"Edit CSS Style"},paste:{"plaintext_mode_stick":"Paste is now in plain text mode. Click again to toggle back to regular paste mode.","plaintext_mode":"Paste is now in plain text mode. Click again to toggle back to regular paste mode. After you paste something you will be returned to regular paste mode.","selectall_desc":"Select All","paste_word_desc":"Paste from Word","paste_text_desc":"Paste as Plain Text"},"paste_dlg":{"word_title":"Use Ctrl+V on your keyboard to paste the text into the window.","text_linebreaks":"Keep Linebreaks","text_title":"Use Ctrl+V on your keyboard to paste the text into the window."},table:{"merge_cells_delta_height":"","merge_cells_delta_width":"","table_delta_height":"","table_delta_width":"","cellprops_delta_height":"","cellprops_delta_width":"","rowprops_delta_height":"","rowprops_delta_width":"",cell:"Cell",col:"Column",row:"Row",del:"Delete Table","copy_row_desc":"Copy Table Row","cut_row_desc":"Cut Table Row","paste_row_after_desc":"Paste Table Row After","paste_row_before_desc":"Paste Table Row Before","props_desc":"Table Properties","cell_desc":"Table Cell Properties","row_desc":"Table Row Properties","merge_cells_desc":"Merge Table Cells","split_cells_desc":"Split Merged Table Cells","delete_col_desc":"Delete Column","col_after_desc":"Insert Column After","col_before_desc":"Insert Column Before","delete_row_desc":"Delete Row","row_after_desc":"Insert Row After","row_before_desc":"Insert Row Before",desc:"Insert/Edit Table"},autosave:{"warning_message":"If you restore the saved content, you will lose all the content that is currently in the editor.\n\nAre you sure you want to restore the saved content?","restore_content":"Restore auto-saved content.","unload_msg":"The changes you made will be lost if you navigate away from this page."},fullscreen:{desc:"Toggle Full Screen Mode"},media:{"delta_height":"","delta_width":"",edit:"Edit Embedded Media",desc:"Insert/Edit Embedded Media"},fullpage:{desc:"Document Properties","delta_width":"","delta_height":""},template:{desc:"Insert Predefined Template Content"},visualchars:{desc:"Show/Hide Visual Control Characters"},spellchecker:{desc:"Toggle Spell Checker",menu:"Spell Checker Settings","ignore_word":"Ignore Word","ignore_words":"Ignore All",langs:"Languages",wait:"Please wait...",sug:"Suggestions","no_sug":"No Suggestions","no_mpell":"No misspellings found.","learn_word":"Learn word"},pagebreak:{desc:"Insert Page Break for Printing"},advlist:{types:"Types",def:"Default","lower_alpha":"Lower Alpha","lower_greek":"Lower Greek","lower_roman":"Lower Roman","upper_alpha":"Upper Alpha","upper_roman":"Upper Roman",circle:"Circle",disc:"Disc",square:"Square"},colors:{"333300":"Dark olive","993300":"Burnt orange","000000":"Black","003300":"Dark green","003366":"Dark azure","000080":"Navy Blue","333399":"Indigo","333333":"Very dark gray","800000":"Maroon",FF6600:"Orange","808000":"Olive","008000":"Green","008080":"Teal","0000FF":"Blue","666699":"Grayish blue","808080":"Gray",FF0000:"Red",FF9900:"Amber","99CC00":"Yellow green","339966":"Sea green","33CCCC":"Turquoise","3366FF":"Royal blue","800080":"Purple","999999":"Medium gray",FF00FF:"Magenta",FFCC00:"Gold",FFFF00:"Yellow","00FF00":"Lime","00FFFF":"Aqua","00CCFF":"Sky blue","993366":"Brown",C0C0C0:"Silver",FF99CC:"Pink",FFCC99:"Peach",FFFF99:"Light yellow",CCFFCC:"Pale green",CCFFFF:"Pale cyan","99CCFF":"Light sky blue",CC99FF:"Plum",FFFFFF:"White"},aria:{"rich_text_area":"Rich Text Area"},wordcount:{words:"Words:"},visualblocks:{desc:'Show/hide block elements'}}});
+			full:"Completo",
+			right:"direita",
+			center:"Centro",
+			left:"Esquerda",
+			align:"Alinhamento"
+		},
+		insertdatetime:{
+			"day_short":"Dom,Seg,Ter,Qua,Qui,Sex,Sab,Dom",
+			"day_long":"Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado,Domingo",
+			"months_short":"Jan,Fev,Mar,Abr,Mai,Jun,Jul,Ago,Set,Out,Nov,Dez",
+			"months_long":"Janeiro,Fevereiro,Março,Abril,Maio,Junho,Julho,Agosto,Setembro,Outubro,Novembro,Dezembro",
+			"inserttime_desc":"Inserir Tempo",
+			"insertdate_desc":"Inserir Data",
+			"time_fmt":"%H:%M:%S",
+			"date_fmt":"%d-%m-%Y"
+		},
+		print:{
+			"print_desc":"Imprimir"
+		},
+		preview:{
+			"preview_desc":"Visualização"
+		},
+		directionality:{
+			"rtl_desc":"Direção Direita para Esquerda",
+			"ltr_desc":"Direção Esquerda para Direita"
+		},
+		layer:{
+			content:"Nova camanda...",
+			"absolute_desc":"Alternar posicionamento absoluto",
+			"backward_desc":"Mover para trás",
+			"forward_desc":"Mover para trás",
+			"insertlayer_desc":"Inserir nova camada"
+		},
+		save:{
+			"save_desc":"Salvar",
+			"cancel_desc":"Cancelar todas as modificações"
+		},
+		nonbreaking:{
+			"nonbreaking_desc":"Inserir espaço em branco"
+		},
+		iespell:{
+			download:"Grafia não detectado. Gostaria de instalar agora?",
+			"iespell_desc":"Verificar ortografia"
+		},
+		advhr:{
+			"delta_height":"",
+			"delta_width":"",
+			"advhr_desc":"Inserir linha horizontal"
+		},
+		emotions:{
+			"delta_height":"",
+			"delta_width":"",
+			"emotions_desc":"Emotions"
+		},
+		searchreplace:{
+			"replace_desc":"Procurar/Substituir",
+			"delta_width":"",
+			"delta_height":"",
+			"search_desc":"Procurar"
+		},
+		advimage:{
+			"delta_width":"",
+			"image_desc":"Inserir/Editar Imagem",
+			"delta_height":""
+		},
+		advlink:{
+			"delta_height":"",
+			"delta_width":"",
+			"link_desc":"Inserir/Editar Link"
+		},
+		xhtmlxtras:{
+			"attribs_delta_height":"",
+			"attribs_delta_width":"",
+			"ins_delta_height":"",
+			"ins_delta_width":"",
+			"del_delta_height":"",
+			"del_delta_width":"",
+			"acronym_delta_height":"",
+			"acronym_delta_width":"",
+			"abbr_delta_height":"",
+			"abbr_delta_width":"",
+			"cite_delta_height":"",
+			"cite_delta_width":"",
+			"attribs_desc":"Inserir/Editar Atributos",
+			"ins_desc":"Inserção",
+			"del_desc":"Eliminação",
+			"acronym_desc":"Acrônimo", // ???Acronym???
+			"abbr_desc":"Abreviação",
+			"cite_desc":"Citação"
+		},
+		style:{
+			"delta_height":"",
+			"delta_width":"",
+			desc:"Editar CSS"
+		},
+		paste:{
+			"plaintext_mode_stick":"Cole agora com o texto simples. Clique novamente para voltar modelo anterior.",
+			"plaintext_mode":"Cole agora com texto simple. Após você colar, será retornado ao modo de cola normal.",
+			"selectall_desc":"Selecionar Tudo",
+			"paste_word_desc":"Colar do Word",
+			"paste_text_desc":"Colar de um bloco de Notas"
+		},
+		"paste_dlg":{
+			"word_title":"Use Ctrl+V para colar o texto nesta janela.",
+			"text_linebreaks":"Manter quebras de linha",
+			"text_title":"Use Ctrl+V para colar o texto nesta janela."
+		},
+		table:{
+			"merge_cells_delta_height":"",
+			"merge_cells_delta_width":"",
+			"table_delta_height":"",
+			"table_delta_width":"",
+			"cellprops_delta_height":"",
+			"cellprops_delta_width":"",
+			"rowprops_delta_height":"",
+			"rowprops_delta_width":"",
+			cell:"Célula",
+			col:"Coluna",
+			row:"Linha",
+			del:"Deletar Tabela",
+			"copy_row_desc":"Copiar linha",
+			"cut_row_desc":"Cortar linha",
+			"paste_row_after_desc":"Colar linha depois",
+			"paste_row_before_desc":"Colar linha antes",
+			"props_desc":"Propriedade da tabela",
+			"cell_desc":"Propriedade da célula",
+			"row_desc":"Propriedade da linha",
+			"merge_cells_desc":"Mesclar células",
+			"split_cells_desc":"Dividir células mescladas",
+			"delete_col_desc":"Deletar Coluna",
+			"col_after_desc":"Inserir coluna depois",
+			"col_before_desc":"Inserir coluna antes",
+			"delete_row_desc":"Deletar linha",
+			"row_after_desc":"Inserir linha depois",
+			"row_before_desc":"Inserir linha antes",
+			desc:"Inserir/Editar tabela"
+		},
+		autosave:{
+			"warning_message":"Se você restaurar o conteúdo salvo, você vai perder todo o conteúdo que está atualmente no editor. \n\nVocê tem certeza que deseja restaurar o conteúdo salvo?",
+			"restore_content":"Restaurar conteúdo auto-salvo.",
+			"unload_msg":"As alterações feitas serão perdidas se você sair desta página."
+		},
+		fullscreen:{
+			desc:"Tela Cheia"
+		},
+		media:{
+			"delta_height":"",
+			"delta_width":"",
+			edit:"Editar vídeo",
+			desc:"Inserir/Editar vídeo"
+		},
+		fullpage:{
+			desc:"Propriedade do documento",
+			"delta_width":"",
+			"delta_height":""
+		},
+		template:{
+			desc:"Inserir conteúdo predefinido"
+		},
+		visualchars:{
+			desc:"Mostrar/Esconder visual das palavras"
+		},
+		spellchecker:{
+			desc:"Alternar Corretor Ortográfico",
+			menu:"Configurações do Corretor Ortográfico",
+			"ignore_word":"Ignorar correção",
+			"ignore_words":"Ignorar todos",
+			langs:"Idiomas",
+			wait:"Por favor espere...",
+			sug:"Sugestões",
+			"no_sug":"Sem sugestão",
+			"no_mpell":"Não foram encontrados erros.",
+			"learn_word":"Ler palavra"
+		},
+		pagebreak:{
+			desc:"Insert Page Break for Printing"
+		},
+		advlist:{
+			types:"Tipos",
+			def:"Padrão",
+			"lower_alpha":"Alpha Minúsculo",
+			"lower_greek":"Greek Minúsculo",
+			"lower_roman":"Roman Minúsculo",
+			"upper_alpha":"Alpha Maiúsculo",
+			"upper_roman":"Roman Maiúsculo",
+			circle:"Circle",
+			disc:"Disco",
+			square:"Quadrado"
+		},
+		colors:{
+			"333300":"Oliva Escuro",
+			"993300":"Laranja Queimado",
+			"000000":"Preto",
+			"003300":"Verde Escuro",
+			"003366":"Azul Escuro",
+			"000080":"Azul-Marinho",
+			"333399":"Índigo",
+			"333333":"Cinza muito escuro",
+			"800000":"Castanho-Avermelhado",
+			FF6600:"Laranja",
+			"808000":"Oliva",
+			"008000":"Verde",
+			"008080":"Azul-Marinho",
+			"0000FF":"Azul",
+			"666699":"Azul Acinzentado",
+			"808080":"Cinza",
+			FF0000:"Vermelha",
+			FF9900:"Amber",
+			"99CC00":"Verde Amarelo",
+			"339966":"Verde mar",
+			"33CCCC":"Turquesa",
+			"3366FF":"Azul Royal",
+			"800080":"Roxo",
+			"999999":"Cinza Médio",
+			FF00FF:"Magenta",
+			FFCC00:"Ouro",
+			FFFF00:"Amarelo",
+			"00FF00":"Limão",
+			"00FFFF":"Aqua",
+			"00CCFF":"Azul-Celeste",
+			"993366":"Marrom",
+			C0C0C0:"Prata",
+			FF99CC:"Rosa",
+			FFCC99:"Pêssego",
+			FFFF99:"Amarelo Claro",
+			CCFFCC:"Verde Pálido",
+			CCFFFF:"Ciano Pálido",
+			"99CCFF":"Azul-Celeste Claro",
+			CC99FF:"Ameixa",
+			FFFFFF:"Branco"
+		},
+		aria:{
+			"rich_text_area":"Marcar texto"
+		},
+		wordcount:{
+			words:"Palavras:"
+		},
+		visualblocks:{
+			desc:'Mostrar/Esconder elementos moldados'
+		}
+	}
+});
